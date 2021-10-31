096f1167610c8de72ce1c626cc19f0b4db7ce0ce
HenokYohannes <henoky096@gmail.com>
2021-10-31 09:58:36 -0400

    add all myfiles

---
+++ src/com/syntax/com08/CLockClockHomeWorkTest.java
@@ -0,0 +1,19 @@
+package com.syntax.com08;
+
+public class CLockClockHomeWorkTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	for(int h=0;h<=23;h++) {
+		for (int m=0; m<59;m++) {
+			for (int s=0;s<59;s++) {
+				System.out.println(h+": "+m+":"+s);
+			}
+		}
+	}
+	
+	
+	}
+
+}
---
+++ src/com/syntax/com08/HomeWorkClock1.java
@@ -0,0 +1,17 @@
+package com.syntax.com08;
+
+public class HomeWorkClock1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+
+		while (hours < 25){
+	        if (hours == 24){
+	            if (minutes == 59){
+	                if (seconds == 60){
+	                    reset(time);
+	
+	}
+
+}
